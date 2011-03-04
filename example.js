5d53f29b8b89c9615f71066fa6a2a140fdc47863
SpenserJ <spenser.e.jones@gmail.com>
2011-03-04 00:41:03 -0700

    Added rough FPS counter
    Obviously drawing the same frames 170 times per second is a waste of CPU cycles, so this will eventually be a debug option for increasing render speed.

--- example.js
+++ example.js
@@ -1,3 +1,6 @@
+var fps = 0, fpsAvg = 0;
+var $fps = $('<div style="position: absolute; bottom: 16px; left: 0px;"></div>');
+var $fpsAvg = $('<div style="position: absolute; bottom: 0px; left: 0px;"></div>');
 // Replace this with a non-jquery onready
 $(function() {
   var canvas = document.getElementById('canvas').getContext('2d');
@@ -7,4 +10,15 @@ $(function() {
     map.render(0, 0, 7, 9, this, canvas);
   }
   tileset.load();
+  $fps.appendTo('body');
+  $fpsAvg.appendTo('body');
 });
+setInterval(function() {
+  $fps.html('FPS: ' + fps);
+  fpsAvg += fps;
+  fps = 0;
+}, 1000);
+setInterval(function() {
+  $fpsAvg.html('Average FPS: ' + fpsAvg / 5);
+  fpsAvg = 0;
+}, 5000);
