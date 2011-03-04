e4a51ba65b17f8a582ed4abf24b6e3e8365ab886
SpenserJ <spenser.e.jones@gmail.com>
2011-03-04 16:17:56 -0700

    Basic sprite rendering

--- example.js
+++ example.js
@@ -2,14 +2,22 @@ var fps = 0, fpsAvg = 0;
 var $fps = $('<div style="position: absolute; bottom: 16px; left: 0px;"></div>');
 var $fpsAvg = $('<div style="position: absolute; bottom: 0px; left: 0px;"></div>');
 // Replace this with a non-jquery onready
+var canvas;
 $(function() {
-  var canvas = document.getElementById('canvas').getContext('2d');
+  canvas = document.getElementById('canvas').getContext('2d');
   var tileset = new Tileset({ path: 'tiles/map.png', tileSpacer: false, tileSize: [32, 32] });
   var map = new Map();
   tileset.callback = function() {
     map.render(0, 0, 7, 9, this, canvas);
   }
   tileset.load();
+  sprite = new Sprite('sprites/character/gramps.png');
+  sprite.callback = function() {
+    sprite.drawSpriteToCanvas(0-8,64,canvas);
+    sprite.drawSpriteToCanvas(192-8,256,canvas,1,1);
+    sprite.drawSpriteToCanvas(224-8,0,canvas,3,2);
+  }
+  sprite.load();
   $fps.appendTo('body');
   $fpsAvg.appendTo('body');
 });
