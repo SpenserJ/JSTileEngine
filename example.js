626b383f237e7a51b86f6654359eea0ddb41c8c5
SpenserJ <spenser.e.jones@gmail.com>
2011-03-04 00:29:00 -0700

    Changed tileset to a legit pokemon one and updated relevant files accordingly

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
 // Replace this with a non-jquery onready
 $(function() {
   var canvas = document.getElementById('canvas').getContext('2d');
-  var tileset = new Tileset({ path: 'tiles/map.png', offset: [1, 1] });
+  var tileset = new Tileset({ path: 'tiles/map.png', tileSpacer: false, tileSize: [32, 32] });
   var map = new Map();
   tileset.callback = function() {
-    map.render(0, 0, 9, 9, this, canvas);
+    map.render(0, 0, 7, 9, this, canvas);
   }
   tileset.load();
 });
