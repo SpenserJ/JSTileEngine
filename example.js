1041cc85cef88f6a70e06bffe5bf5fe516aba696
SpenserJ <spenser.e.jones@gmail.com>
2011-03-04 18:06:43 -0700

    Added basic functionality for cleaning up modified tiles (instead of full redraws)

--- example.js
+++ example.js
@@ -2,12 +2,12 @@ var fps = 0, fpsAvg = 0;
 var $fps = $('<div style="position: absolute; bottom: 16px; left: 0px;"></div>');
 var $fpsAvg = $('<div style="position: absolute; bottom: 0px; left: 0px;"></div>');
 // Replace this with a non-jquery onready
-var canvas;
+var canvas, tileset, map;
 $(function() {
   canvas = document.getElementById('canvas').getContext('2d');
   
-  var tileset = new Tileset({ path: 'tiles/map.png', tileSpacer: false, tileSize: [32, 32] });
-  var map = new Map();
+  tileset = new Tileset({ path: 'tiles/map.png', tileSpacer: false, tileSize: [32, 32] });
+  map = new Map();
   tileset.callback = function() {
     map.render(0, 0, 7, 9, this, canvas);
   }
