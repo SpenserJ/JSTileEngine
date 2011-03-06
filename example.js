69725a7cbde7d1e101630d7bec44444bc3c2f381
SpenserJ <spenser.e.jones@gmail.com>
2011-03-06 03:57:47 -0700

    Goodbye gramps, hello walking ladies

--- example.js
+++ example.js
@@ -12,21 +12,15 @@ $(function() {
     libraries.canvas.renderBase(0, 0, 8, 14);
     libraries.canvas.renderOverlay(0, 0, 8, 14);
     
-    players['npc-amy'] = new Player({ spritePath: 'sprites/character/amy.png', coords: [1, 2], libraries: libraries, playerID: 'npc-amy' });
+    players['npc-amy'] = new Player({ spritePath: 'sprites/character/amy.png', coords: [1, 4], libraries: libraries, playerID: 'npc-amy' });
     setInterval(function() {
-      //players['npc-amy'].move(playerMovement[Math.floor(Math.random() * 4)]);
+      players['npc-amy'].move(playerMovement[Math.floor(Math.random() * 4)]);
     }, 1000);
     
     players['npc-misty'] = new Player({ spritePath: 'sprites/character/misty.png', coords: [6, 13], spriteState: 1, libraries: libraries, playerID: 'npc-misty' });
     setInterval(function() {
-      //players['npc-misty'].move(playerMovement[Math.floor(Math.random() * 4)]);
+      players['npc-misty'].move(playerMovement[Math.floor(Math.random() * 4)]);
     }, 1000);
-    
-    sprite3 = new Sprite({ path: 'sprites/character/gramps.png', tileSize: [32, 32], libraries: libraries });
-    sprite3.callback = function() {
-      sprite3.drawSpriteToCanvas(4,11,canvas,3,2);
-    }
-    sprite3.load();
     
     players['player-1'] = new Player({ libraries: libraries, playerID: 'player-1' });
     
