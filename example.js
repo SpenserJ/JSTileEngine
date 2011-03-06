d053568c0fb941aee5c7845983c3c3fc2819420b
SpenserJ <spenser.e.jones@gmail.com>
2011-03-06 01:35:25 -0700

    Working on map/sprite drawing. Rough non-working implementation.
    Committing for backup sake

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ var fps = 0, fpsAvg = 0;
 var $fps = $('<div style="position: absolute; bottom: 16px; left: 0px;"></div>');
 var $fpsAvg = $('<div style="position: absolute; bottom: 0px; left: 0px;"></div>');
 // Replace this with a non-jquery onready
-var canvas, tileset, map, player, npc = {}, playerMovement = ['left', 'up', 'right', 'down'];
+var canvas, tileset, map, player, players = {}, currentPlayer = 1, playersOnTile = [], playerMovement = ['left', 'up', 'right', 'down'];
 $(function() {
   canvas = document.getElementById('canvas').getContext('2d');
   
@@ -11,14 +11,14 @@ $(function() {
   tileset.callback = function() {
     map.render(0, 0, 8, 14, this, canvas);
     
-    npc.amy = new Player({ spritePath: 'sprites/character/amy.png', coords: [1, 7], map: map, canvas: canvas, tileset: tileset });
+    players['npc-amy'] = new Player({ spritePath: 'sprites/character/amy.png', coords: [1, 2], map: map, canvas: canvas, tileset: tileset, playerID: 'npc-amy' });
     setInterval(function() {
-      npc.amy.move(playerMovement[Math.floor(Math.random() * 4)]);
+      //players['npc-amy'].move(playerMovement[Math.floor(Math.random() * 4)]);
     }, 1000);
     
-    npc.misty = new Player({ spritePath: 'sprites/character/misty.png', coords: [6, 13], spriteState: 1, map: map, canvas: canvas, tileset: tileset });
+    players['npc-misty'] = new Player({ spritePath: 'sprites/character/misty.png', coords: [6, 13], spriteState: 1, map: map, canvas: canvas, tileset: tileset, playerID: 'npc-misty' });
     setInterval(function() {
-      npc.misty.move(playerMovement[Math.floor(Math.random() * 4)]);
+      //players['npc-misty'].move(playerMovement[Math.floor(Math.random() * 4)]);
     }, 1000);
     
     sprite3 = new Sprite('sprites/character/gramps.png', [32, 32], map, tileset);
@@ -27,16 +27,17 @@ $(function() {
     }
     sprite3.load();
     
-    player = new Player({ map: map, canvas: canvas, tileset: tileset });
+    players['player-1'] = new Player({ map: map, canvas: canvas, tileset: tileset, playerID: 'player-1' });
     
     $('body').keydown(function(e) {
       switch (e.keyCode) {
-        case 37: player.move('left');  break;
-        case 38: player.move('up');    break;
-        case 39: player.move('right'); break;
-        case 40: player.move('down');  break;
+        case 37: players['player-1'].move('left');  break;
+        case 38: players['player-1'].move('up');    break;
+        case 39: players['player-1'].move('right'); break;
+        case 40: players['player-1'].move('down');  break;
       }
     });
+    $('body');
   }
   tileset.load();
   
